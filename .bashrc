fa7881c6a3969a2dc83317999df73aa40e50db4e
LE Van Tuan <levantuan2312@gmail.com>
2020-04-09 22:57:01 +0200

    [pyenv] install

--- .bashrc
+++ .bashrc
@@ -2,3 +2,7 @@ function _go {
 	cd `find /mnt/d/Programming/ -name *$1* -print -quit` 
 }
 alias go=_go
+PATH=~/.local/bin/:$PATH
+export PATH="/home/lc888093/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
---
+++ re_init.sh
@@ -0,0 +1,8 @@
+sudo apt install python
+sudo apt-get update
+sudo apt install pip
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+			 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+			 xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+curl https://pyenv.run | bash
+
