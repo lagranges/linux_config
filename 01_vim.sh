c4e50d907581e7ee61a8cb5fab645e17a0f65dfb
LE Van Tuan <levantuan2312@gmail.com>
2020-08-20 23:26:09 +0200

    [vim] add syntastic

--- 01_vim.sh
+++ 01_vim.sh
@@ -3,3 +3,5 @@ mkdir -p ~/.vim/colors
 cd ~/.vim/colors
 curl -O https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
 echo "so ~/linux_config/vim/vimrc" >> ~/.vimrc
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle && git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
