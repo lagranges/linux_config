accef9463184afe0f2f2edd3f3770008dccdc373
LE Van Tuan <levantuan2312@gmail.com>
2020-08-22 17:11:27 +0200

    [init_scripts][bash] install xclip

--- init_scripts/00_bash.sh
+++ init_scripts/00_bash.sh
@@ -3,3 +3,4 @@ sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
 			 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 			 xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
 curl https://pyenv.run | bash
+sudo apt-get install xclip
